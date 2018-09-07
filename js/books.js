a216d582c9af0acdc34cf4de27a23f11a627fde2
Joke Heyndels <jo_hannah@outlook.com>
2018-09-07 16:36:42 +0200

    formatting html and js

--- js/books.js
+++ js/books.js
@@ -1,159 +1,159 @@
-$( document ).ready(function() {
-queue()
-  .defer(d3.json, "data/mylibrary.json")
-  .await(charts);
+$(document).ready(function() {
+  queue()
+    .defer(d3.json, "data/mylibrary.json")
+    .await(charts);
 
-function charts(error, myLibraryData) {
-  let ndx = crossfilter(myLibraryData);  
-  
-  
-  showGenres(ndx);
-  showAuthors(ndx);
-  showProtGender(ndx);
-  showLanguage(ndx);
-  showSeries(ndx);
-  showNumberOfSeries(ndx);
-  showTable(ndx)
-  
-  dc.renderAll()
-}
+  function charts(error, myLibraryData) {
+    let ndx = crossfilter(myLibraryData);
 
-let myTransTimeVariable = 500
-var myWidthVariable = 250
+    showGenres(ndx);
+    showAuthors(ndx);
+    showProtGender(ndx);
+    showLanguage(ndx);
+    showSeries(ndx);
+    showNumberOfSeries(ndx);
+    showTable(ndx);
 
-function showGenres(ndx) {
-  var dim = ndx.dimension(dc.pluck("genre"));
-  var group = dim.group();
+    dc.renderAll();
+  }
 
-  dc.pieChart("#genres")
-    .height(300)
-    .width(myWidthVariable*1.5)
-    .radius(myWidthVariable/2)
-    .transitionDuration(myTransTimeVariable)
-    .dimension(dim)
-    .group(group)
-    .legend(dc.legend().gap(7))
-}
+  let myTransTimeVariable = 500;
+  var myWidthVariable = 250;
 
-function showProtGender(ndx) {
-  var dim = ndx.dimension(dc.pluck("Gender main character"));
-  var group = dim.group();
+  function showGenres(ndx) {
+    var dim = ndx.dimension(dc.pluck("genre"));
+    var group = dim.group();
 
-  dc.pieChart("#protGender")
-    .height(300)
-    .width(myWidthVariable*1.5)
-    .radius(myWidthVariable/2)
-    .transitionDuration(myTransTimeVariable)
-    .dimension(dim)
-    .group(group)
-    .legend(dc.legend())
-}
+    dc.pieChart("#genres")
+      .height(300)
+      .width(myWidthVariable * 1.5)
+      .radius(myWidthVariable / 2)
+      .transitionDuration(myTransTimeVariable)
+      .dimension(dim)
+      .group(group)
+      .legend(dc.legend().gap(7));
+  }
 
-function showAuthors(ndx) {
-  var dim = ndx.dimension(dc.pluck("author"));
-  var group = dim.group();
+  function showProtGender(ndx) {
+    var dim = ndx.dimension(dc.pluck("Gender main character"));
+    var group = dim.group();
 
-  dc.barChart("#authors")
-    .width(myWidthVariable*3)
-    .height(300)
-    .margins({
-      top: 10,
-      right: 10,
-      bottom: 30,
-      left: 30
-    })
-    .dimension(dim)
-    .group(group)
-    .transitionDuration(myTransTimeVariable)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .elasticY(true)
-    .xAxisLabel("Author")
-    .yAxisLabel("Books")
-    .yAxis().ticks(4)
-}
+    dc.pieChart("#protGender")
+      .height(300)
+      .width(myWidthVariable * 1.5)
+      .radius(myWidthVariable / 2)
+      .transitionDuration(myTransTimeVariable)
+      .dimension(dim)
+      .group(group)
+      .legend(dc.legend());
+  }
 
-function showLanguage(ndx) {
-  var dim = ndx.dimension(dc.pluck("language"));
-  var group = dim.group();
+  function showAuthors(ndx) {
+    var dim = ndx.dimension(dc.pluck("author"));
+    var group = dim.group();
 
-  dc.barChart("#languages")
-    .width(myWidthVariable)
-    .height(500)
-    .margins({
-      top: 10,
-      right: 10,
-      bottom: 30,
-      left: 30
-    })
-    .dimension(dim)
-    .group(group)
-    .transitionDuration(myTransTimeVariable)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .elasticY(false)
-    .xAxisLabel("Language")
-    .yAxisLabel("Books")
-    .yAxis().ticks(10)
-}
+    dc.barChart("#authors")
+      .width(myWidthVariable * 3)
+      .height(300)
+      .margins({
+        top: 10,
+        right: 10,
+        bottom: 30,
+        left: 30
+      })
+      .dimension(dim)
+      .group(group)
+      .transitionDuration(myTransTimeVariable)
+      .x(d3.scale.ordinal())
+      .xUnits(dc.units.ordinal)
+      .elasticY(true)
+      .xAxisLabel("Author")
+      .yAxisLabel("Books")
+      .yAxis()
+      .ticks(4);
+  }
 
+  function showLanguage(ndx) {
+    var dim = ndx.dimension(dc.pluck("language"));
+    var group = dim.group();
 
-function showSeries(ndx) {
-  var dim = ndx.dimension(dc.pluck("series"));
-  var group = dim.group();
+    dc.barChart("#languages")
+      .width(myWidthVariable)
+      .height(500)
+      .margins({
+        top: 10,
+        right: 10,
+        bottom: 30,
+        left: 30
+      })
+      .dimension(dim)
+      .group(group)
+      .transitionDuration(myTransTimeVariable)
+      .x(d3.scale.ordinal())
+      .xUnits(dc.units.ordinal)
+      .elasticY(false)
+      .xAxisLabel("Language")
+      .yAxisLabel("Books")
+      .yAxis()
+      .ticks(10);
+  }
 
+  function showSeries(ndx) {
+    var dim = ndx.dimension(dc.pluck("series"));
+    var group = dim.group();
 
-  dc.barChart("#seriesOrNot")
-    .width(myWidthVariable)
-    .height(500)
-    .margins({
-      top: 10,
-      right: 10,
-      bottom: 30,
-      left: 30
-    })
-    .dimension(dim)
-    .group(group)
-    .transitionDuration(myTransTimeVariable)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .elasticY(false)
-    .xAxisLabel("Series")
-    .yAxisLabel("Books")
-    .yAxis().ticks(10)
-}
+    dc.barChart("#seriesOrNot")
+      .width(myWidthVariable)
+      .height(500)
+      .margins({
+        top: 10,
+        right: 10,
+        bottom: 30,
+        left: 30
+      })
+      .dimension(dim)
+      .group(group)
+      .transitionDuration(myTransTimeVariable)
+      .x(d3.scale.ordinal())
+      .xUnits(dc.units.ordinal)
+      .elasticY(false)
+      .xAxisLabel("Series")
+      .yAxisLabel("Books")
+      .yAxis()
+      .ticks(10);
+  }
 
-function showNumberOfSeries(ndx) {
-  var totalSeries = ndx.groupAll().reduce(
-    function (p, v) {
-      if (v.number === 1) {
-        p.isSeries++
+  function showNumberOfSeries(ndx) {
+    var totalSeries = ndx.groupAll().reduce(
+      function(p, v) {
+        if (v.number === 1) {
+          p.isSeries++;
+        }
+        return p;
+      },
+      function(p, v) {
+        if (v.number === 1) {
+          p.isSeries--;
+        }
+        return p;
+      },
+      function() {
+        return {
+          isSeries: 0
+        };
       }
-      return p;
-    },
-    function (p, v) {
-      if (v.number === 1) {
-        p.isSeries--;
-      }
-      return p;
-    },
-    function () {
-      return {
-        isSeries: 0
-      };
-    },
-  )
+    );
 
-  dc.numberDisplay("#numberOfSeries")
-    .formatNumber(d3.format("1"))
-    .valueAccessor(function (d) {
-      return (d.isSeries);
-    })
-    .group(totalSeries)
-    .transitionDuration(0)
-}
+    dc.numberDisplay("#numberOfSeries")
+      .formatNumber(d3.format("1"))
+      .valueAccessor(function(d) {
+        return d.isSeries;
+      })
+      .group(totalSeries)
+      .transitionDuration(0);
+  }
 
-/*Table function completed with the help of Nakita of the Tutor team.
+  /*Table function completed with the help of Nakita of the Tutor team.
 My original code on last save: 
 
 function showTable(ndx) {
@@ -191,22 +191,19 @@ function showTable(ndx) {
 }
 
 */
-function showTable(ndx) {
-  var dim = ndx.dimension(dc.pluck('author'));
-  dc.dataTable("#allBooks")
-  .dimension(dim)
-  .group(function(d) {
-    return '';
-  })
-  .columns([
-    "title",
-    "author",
-    "language",
-    "genre",
-    ])
-  .size(214)
-  .sortBy(function(d) { return d.author; })
-  .order(d3.ascending)
-  .transitionDuration(myTransTimeVariable)
-}
+  function showTable(ndx) {
+    var dim = ndx.dimension(dc.pluck("author"));
+    dc.dataTable("#allBooks")
+      .dimension(dim)
+      .group(function(d) {
+        return "";
+      })
+      .columns(["title", "author", "language", "genre"])
+      .size(214)
+      .sortBy(function(d) {
+        return d.author;
+      })
+      .order(d3.ascending)
+      .transitionDuration(myTransTimeVariable);
+  }
 });
